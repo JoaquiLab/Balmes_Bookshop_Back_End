ed3fad41927c67ece956fd4092edac46d18c0ef6
joaquiLab <121823460JoaquiLab@users.noreply.github.com>
2026-05-16 13:43:49 +0200

    feature(root-app): started the addcion of microservice arquitecture

--- src/books/book.controller.ts
+++ src/books/book.controller.ts
@@ -1,15 +1,22 @@
 import { Get, Query, Controller, Inject } from '@nestjs/common';
 import type { Book, GridDataResponse } from './interfaces/categories.interface';
 import { BookService } from './infrastructure/books.service';
+import { EXAMPLE_DATA } from 'src/mock/books-database';
+import { CategoryTreeNode } from 'src/categories/interfaces/categories.interface';
 
 @Controller('books')
 export class BookController {
+  categories: CategoryTreeNode[] = EXAMPLE_DATA;
   constructor(private readonly bookService: BookService){}
 
-  //GetBooks
+  /**
+   * /books
+   * /books?name=<book name>
+   * @param name 
+   * @returns 
+   */
   @Get()
   findAll(@Query('name') name: string): GridDataResponse {
-    // /books?name?=''||'abc'
     const books: Book[] = this.bookService.getAllBooks();
     if (name == '') {
       return {
@@ -35,4 +42,15 @@ export class BookController {
       },
     };
   }
+
+  /**
+   * /books/categoryTree
+   */
+  @Get('categories')
+  findCategories(): CategoryTreeNode[]{
+    return this.categories
+  }
 }
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,19 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+
+//TODO: Miscroservices implementation
+// async function bootstrap() {
+//   const app = await NestFactory.createMicroservice<MicroserviceOptions>(UsersModule, {
+//     transport: Transport.TCP,
+//     options: {
+//       host:'localhost',
+//       port: 3001
+//     }
+//   });
+//   // app.enableCors()
+//   await app.listen();
+//   console.log('User Service is listening at port 3001')
+// }
+// bootstrap();
