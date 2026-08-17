172dde2af7945ade45f4013549ca15aec163800b
joaquiLab <121823460JoaquiLab@users.noreply.github.com>
2026-08-17 18:49:51 +0200

    fix(book-service): I delete the function of sortwith it's test 'cause it'll be useless with the database connection

--- src/books/infrastructure/books.service.ts
+++ src/books/infrastructure/books.service.ts
@@ -15,41 +15,6 @@ export class BookService {
         book.title.toUpperCase().includes(name.toUpperCase()),
       );
     }
-    //Apply the sortType
-    const sortedList = this.sortBooks(bookList, sortType);
-    return sortedList;
-  }
-
-  /**
-   * Function to sort a list in 3 differents ways:
-   * - 1 High Price to low price
-   * - 2 Low Price to high price
-   * - 3 newest to lowest
-   * @param bookList
-   * @param sortType
-   */
-  sortBooks(bookList: Book[], sortType: number): Book[] {
-    //High price to Low price
-    if (sortType === 1) {
-      bookList.sort((a, b) => {
-        return a.price - b.price;
-      });
-    }
-    //Low price to High price
-    else if (sortType === 2) {
-      bookList.sort((a, b) => {
-        return b.price - a.price;
-      });
-    }
-    //New to old
-    else {
-      bookList.sort((a, b) => {
-        if (a.onSaleDate === b.onSaleDate) return 0;
-        else if (a.onSaleDate < b.onSaleDate) return -1;
-        else return 1;
-      });
-    }
-
     return bookList;
   }
 }
