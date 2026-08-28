5d0d63b946f53b07499db9c2033323aa49736b00
joaquiLab <121823460JoaquiLab@users.noreply.github.com>
2026-08-28 10:47:39 +0200

    reafactor(User service: I move the User mocked data to the mock folder

---
+++ src/users/mock/user-mock-data.ts
@@ -0,0 +1,16 @@
+import { User } from "../interfaces/user-interface";
+
+export const databaseUsers: User[] = [
+    {
+      id: '1',
+      email: 'test@test.com',
+      password: '123456',
+      token: 'fake-jwt-token-123',
+    },
+    {
+      id: '2',
+      email: 'tasfiaAli@beatiful.com',
+      password: 'arabe',
+      token: 'fake-jwt-token-tasfi',
+    },
+  ];
