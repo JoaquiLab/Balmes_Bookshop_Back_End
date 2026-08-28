3591dfd7768c6d3f51c4868dde5f82e57e36d92b
joaquiLab <121823460JoaquiLab@users.noreply.github.com>
2026-08-28 19:46:18 +0200

    feature(User service): I add TODOS to the service functions

--- src/users/infrastructure/user.service.ts
+++ src/users/infrastructure/user.service.ts
@@ -1,39 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '../interfaces/user-interface';
+import { Injectable, Logger } from '@nestjs/common';
+import { UserI } from '../interfaces/user-interface';
+import { User } from '../models/user_model';
 
 @Injectable({})
 export class UserService {
-  
-  public set User(userToCreate: User): User | null {
-    // check if the users already exists
-    // check if the user data is correct
+  logger = new Logger('[USER_SERVICE]');
+
+  public createNewUser(userToCreate: User): User | null {
+    // TODO:check if the users already exists
+    // TODO:check if the user data is correct
     // call backend function to create the user
-    // return the new user
     // all must be done in a try catch
-    throw new InternalServerErrorException();
-  }
-  
-  public set delete(userIdToDelete : string): void {
-    // check if the user exists
-    // access tothe database
-    // filter by the id
-    // delete the user  
-  }
-  
-  public set modifyUser(userId : string, paramToModify: string) {
-    //check if the user exists
-    //if not exists return an error
-    //if the user exists, check if the param exists in the model
-    // if the param not exists return an error
-    // if the param exists call function to accessthe database and modify
-  }
-  
-  public get userExists(userId) : boolean {
-    // call to internal function to check if the user exists
-    return false 
+    try {
+      const newUser = new User(
+        userToCreate.name,
+        userToCreate._email,
+        userToCreate._password,
+      );
+      // return the new user
+      return newUser;
+    } catch (error) {
+      this.logger.error('The user could not be created');
+      return null;
+    }
   }
 
-  
+  public set deleteUser(userIdToDelete: string): void {
+    // TODO: check if the user exists
+    // TODO: access tothe database
+    // TODO: filter by the id
+    // TODO: delete the user
+  }
+
+  public set modifyUser(userId: string, paramToModify: string) {
+    // TODO: check if the user exists
+    // TODO: if not exists return an error
+    // TODO: if the user exists, check if the param exists in the model
+    // TODO: if the param not exists return an error
+    // TODO: if the param exists call function to accessthe database and modify
+  }
+
+  public get userExists(userId): boolean {
+    // TODO: call to internal function to check if the user exists
+    return false;
+  }
 }
-  
-  
